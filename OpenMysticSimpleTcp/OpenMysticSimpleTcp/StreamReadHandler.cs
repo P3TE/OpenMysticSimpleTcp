92fd74e63b31b538ce5c7309f36765d570902780
P3TE <thisisntanemailaccount@gmail.com>
2021-04-05 13:23:57 +1000

    Ii work

---
+++ OpenMysticSimpleTcp/OpenMysticSimpleTcp/StreamReadHandler.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace OpenMysticSimpleTcp.ReadWrite
+{
+    public class StreamReadHandler
+    {
+
+        private const int _DefaultThreadJoinTimeoutMilliseconds = 1000; //1 Second.
+        private const int _DefaultBufferSize = 1024; //Bytes.
+        
+        private readonly int threadJoinDuration;
+        private readonly int bufferSize;
+
+        private Stream dataStream;
+        
+        private Thread readThread;
+        private bool threadRunning = false;
+
+        private ConcurrentQueue<byte[]> receivedDataQueue = new ConcurrentQueue<byte[]>();
+
+        public StreamReadHandler(Stream dataStream, int threadJoinDurationMilliseconds = _DefaultThreadJoinTimeoutMilliseconds)
+        {
+
+            this.dataStream = dataStream;
+            this.threadJoinDuration = threadJoinDurationMilliseconds;
+            
+            threadRunning = true;
+            readThread = new Thread(ReadThreadHandle)
+            {
+                IsBackground = true
+            };
+            readThread.Start();
+        }
+
+        private void ReadThreadHandle()
+        {
+            while (threadRunning)
+            {
+                
+
+            }
+        }
+
+
+        private void Read()
+        {
+            dataStream.Read()
+        }
+
+        public bool GetLatestData(out byte[] data)
+        {
+            return receivedDataQueue.TryDequeue(out data);
+        }
+
+        public bool StopThread()
+        {
+            if (!threadRunning)
+            {
+                //We are already shutting down, ignore.
+                return true;
+            }
+            
+            try
+            {
+                dataStream.Close();
+            }
+            catch (Exception)
+            {
+                //We're closing anyway, ignore this exception.
+            }
+
+            bool threadShutdownSuccess = true;
+            
+            if (Thread.CurrentThread != readThread)
+            {
+                //If we are not the thread we are wanting to close, join the thread.
+                threadShutdownSuccess = readThread.Join(threadJoinDuration);
+            }
+
+            return threadShutdownSuccess;
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# OpenMysticSimpleTcp
+# OpenMysticSimpleTcp #
 A simple library for helping create TCP connections. Written in C#
+
+### Useage ###
+
+#### Events ####
+
+There are two types of events that will occur for the main thread:
+- TCP update (E.G. Connection Established, Connection Lost, ... )
+- New data received.
+
+These events will be received in order of occurrence.
+
+### Design Goals ###
+
+#### Multithreading ####
+The library uses blocking TCP.  
+The library is designed to accomodate the design pattern of one main thread
+that has an update loop that gets called at regular intervals. During
+this update the thread will check whether it has received any new data over
+TCP.  
+  
+- The TCP read will be handled on a dedicated thread storing received data
+to be handled by the main thread in the next update.  
+  
+- The TCP write will be handled on a dedicated thread where the main thread
+sends data to write which is handled concurrently.  
+  
+- The initial connections will also be on a dedicated thread.
+
+
+#### Memory Management ####
+C# is a garbage collected language and as such all garbage will be collected until an unspecified time 
+at which point the garbage collector will clean up the memory.    
+  
+- The goal here will be to minimize GC by reusing memory.
+- Introduce a maximum limit for memory stored on the Read thread. 
+
+
